a08cefddacfa2b82193e88cf09b1765e24ee76ee
Robin <24991308+Rolami@users.noreply.github.com>
2022-11-17 11:31:36 +0100

    Added classes +

---
+++ RabbitEyeBank/UI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitEyeBank
+{
+    internal class UI
+    {
+
+        public static string logotyp = @$"
+  _______________________________       |                            {DateTime.Now}
+  \______   \_   _____/\______   \      |   
+   |       _/|    __)_  |    |  _/      |   
+   |    |   \|        \ |    |   \      |   
+   |____|_  /_______  / |______  /      |   
+          \/        \/         \/       |     
+                                                  ";
+    }
+}
