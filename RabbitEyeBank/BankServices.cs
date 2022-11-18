a7e9ad39a9c9a5f979898fed25da2a6b19d507f4
Robin <24991308+Rolami@users.noreply.github.com>
2022-11-18 11:11:48 +0100

    Start of branch

---
+++ RabbitEyeBank/BankServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitEyeBank
+{
+    internal class BankServices
+    {
+        //create login
+        static void Login(string username = "admin", string password = "password")
+        {
+            //ta input
+            //kontrollera mot variabel
+            //om fel, avbryt
+            //om rätt, fråga om lösen
+            
+
+        }
+    }
+
+}
