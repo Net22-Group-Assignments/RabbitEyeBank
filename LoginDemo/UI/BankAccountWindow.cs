4ce941dadad4d52997135a323efae43a1b7a7413
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-19 23:34:12 +0100

    refactor(ui): move bank account window to own file

---
+++ LoginDemo/UI/BankAccountWindow.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console;
+
+namespace LoginDemo.UI;
+
+public class BankAccountWindow : IWindow
+{
+    public void Show()
+    {
+        AnsiConsole.Clear();
+        WindowManager.showWindowStack();
+        AnsiConsole.WriteLine($"Level {WindowManager.Level}");
+        AnsiConsole.WriteLine("Customers Account and balance screen here:");
+        AnsiConsole.WriteLine("Press a key to go back. In real app Customer would choose exit.");
+        Console.ReadKey();
+    }
+}
--- LoginDemo/UI/WindowManager.cs
+++ LoginDemo/UI/WindowManager.cs
@@ -159,19 +159,4 @@ namespace LoginDemo.UI
             Console.ReadKey();
         }
     }
-
-    public class BankAccountWindow : IWindow
-    {
-        public void Show()
-        {
-            AnsiConsole.Clear();
-            WindowManager.showWindowStack();
-            AnsiConsole.WriteLine($"Level {WindowManager.Level}");
-            AnsiConsole.WriteLine("Customers Account and balance screen here:");
-            AnsiConsole.WriteLine(
-                "Press a key to go back. In real app Customer would choose exit."
-            );
-            Console.ReadKey();
-        }
-    }
 }
