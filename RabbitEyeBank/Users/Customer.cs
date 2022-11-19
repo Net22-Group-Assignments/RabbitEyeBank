c8ed4280082cc6b9609e53f5ab161ca7ad158684
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-19 22:35:05 +0100

    refactor(Users): rename User class to Customer

--- RabbitEyeBank/Users/Customer.cs
+++ RabbitEyeBank/Users/Customer.cs
@@ -1,4 +1,41 @@
-﻿namespace RabbitEyeBank.User_Management
+﻿using RabbitEyeBank.Money;
+
+namespace RabbitEyeBank.Users
 {
-    public class Customer { }
+    /// TODO Should this be named customer instead? Or should this be user and customer inherits this + gets the bankaccount?
+    /// <summary>
+    /// Represents a user/customer.
+    /// </summary>
+    public class Customer
+    {
+        //private string id; //variable camelCase
+        private int loginAttempts;
+
+        //public Guid  { get; set; }
+        public string FirstName { get; set; } // property PascalCase
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Can make the user inactive if failed.
+        /// Usually used with incrementation.
+        /// </summary>
+        public int LoginAttempts
+        {
+            get => loginAttempts;
+            set
+            {
+                loginAttempts = value;
+                if (loginAttempts >= 3)
+                {
+                    IsActive = false;
+                }
+            }
+        }
+
+        public List<BankAccount> BankAccountList = new();
+    }
 }
