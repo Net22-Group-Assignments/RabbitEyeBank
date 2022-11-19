477124dc06e6b37b17858df59758018556fce396
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-19 13:08:52 +0100

    feat(money): add global user list

--- RabbitEyeBank/BankServices.cs
+++ RabbitEyeBank/BankServices.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitEyeBank.Users;
 
 namespace RabbitEyeBank
 {
-
-    public class BankServices
+    public static class BankServices
     {
+        /// <summary>
+        /// Stores all users/customers in the bank.
+        /// </summary>
+        public static List<User> UserList { get; } = new();
 
-       
+        // TODO the login method should return a value to the caller so the ui can give the right message to the user.
         public static void Login(string userName = "admin", string password = "password")
         {
             //kontrollera mot variabel
@@ -18,9 +22,6 @@ namespace RabbitEyeBank
             //om rätt, fråga om lösen
 
             //skapa List string username
-            
-
         }
     }
-
 }
