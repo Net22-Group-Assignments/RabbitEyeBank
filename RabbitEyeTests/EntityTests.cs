aee10700c2cbeff68db21c1cc31c00afb9448de6
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-24 12:43:18 +0100

    test: add xunit tests for new entity versions

---
+++ RabbitEyeTests/EntityTests.cs
@@ -0,0 +1,35 @@
+﻿using RabbitEyeBank.Money;
+using RabbitEyeBank.Services;
+using RabbitEyeBank.Users;
+
+namespace RabbitEyeTests;
+
+public class EntityTests
+{
+    private readonly Customer customer1;
+    private readonly Customer customer2;
+    private readonly BankAccount bankAccount1;
+    private readonly BankAccount bankAccount2;
+    private readonly BankAccount bankAccount3;
+
+    public EntityTests()
+    {
+        customer1 = new Customer("Alice", "Allison", "alice", "alice", true);
+        customer2 = new Customer("Bob", "Roberts", "bob", "bob", true);
+        BankServices.AddCustomer(customer1);
+        BankServices.AddCustomer(customer2);
+        bankAccount1 = new BankAccount("1234", "savings", 100m, new Currency(), customer1);
+        bankAccount2 = new BankAccount("5678", "loan", 200m, new Currency(), customer1);
+        bankAccount3 = new BankAccount("9012", "slush-fund", 300m, new Currency(), customer2);
+        AccountService.AddBankAccount(bankAccount1);
+        AccountService.AddBankAccount(bankAccount2);
+        AccountService.AddBankAccount(bankAccount3);
+    }
+
+    [Fact]
+    public void CustomersBankAccountPropertyShouldFindOwnAccounts()
+    {
+        IReadOnlyList<BankAccount> accounts = AccountService.BankAccountsByCustomer(customer1);
+        Assert.Equal(new[] { bankAccount1, bankAccount2 }, accounts);
+    }
+}
--- RabbitEyeTests/MoneyTransferTests.cs
+++ RabbitEyeTests/MoneyTransferTests.cs
@@ -1,10 +1,18 @@
 ﻿using RabbitEyeBank.Money;
+using RabbitEyeBank.Users;
 
 namespace RabbitEyeTests;
 
 public class MoneyTransferTests
 {
-    private readonly BankAccount bankAccount = new("1", "A", 0m, new Currency());
+    private readonly Customer customer;
+    private readonly BankAccount bankAccount;
+
+    public MoneyTransferTests()
+    {
+        customer = new Customer("User", "Name", "username", "password", true);
+        bankAccount = new("1", "A", 0m, new Currency(), customer);
+    }
 
     [Fact]
     public void WithdrawalWhenAmountTooLittle_ThrowsException()
