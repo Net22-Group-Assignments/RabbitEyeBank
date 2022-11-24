a9329a0f9ae30107055559ab854bb2e8c1d2812f
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-24 16:30:09 +0100

    feat: add service class for moneytransfers

---
+++ RabbitEyeBank/Services/MoneyTransferService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+using RabbitEyeBank.Money;
+
+namespace RabbitEyeBank.Services;
+
+public static class MoneyTransferService
+{
+    private static readonly ConcurrentQueue<MoneyTransfer> TransferQueue = new();
+    private static readonly List<MoneyTransfer> TransferLog = new();
+
+    public static void RegisterTransfer(MoneyTransfer transfer)
+    {
+        transfer.Register();
+        TransferQueue.Enqueue(transfer);
+    }
+
+    public static void CompleteTransfer()
+    {
+        MoneyTransfer transfer;
+        TransferQueue.TryDequeue(out transfer);
+        if (transfer.Status == TransferStatus.Pending)
+        {
+            transfer.ToAccount.Deposit(transfer.Amount);
+            transfer.Complete();
+        }
+        TransferLog.Add(transfer);
+    }
+}
