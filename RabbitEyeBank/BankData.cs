858fe34ecd1344bb679f7020a1d82f1f7f0f84b2
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-24 01:37:53 +0100

    feat: add simple account number generation
    closes #41

--- RabbitEyeBank/BankData.cs
+++ RabbitEyeBank/BankData.cs
@@ -1,45 +1,62 @@
 ﻿using System.Globalization;
+using System.Security.Cryptography;
+using System.Text;
+using Bogus;
 using RabbitEyeBank.Money;
+using Serilog;
 
-namespace RabbitEyeBank;
-
-public static class BankData
+namespace RabbitEyeBank
 {
-    private static readonly string[] cultureNames = { "en-US", "be-BE", "th-TH", "sv-SE" };
-    public static readonly Dictionary<CurrencyISO, Currency> CurrencyDictionary;
-
-    // TODO Should the dictionary be stored somewhere else, like BankService?
-
-    /// <summary>
-    /// Initializes the dictionary by connecting the currency type
-    /// to the currency symbol in the CultureInfo class.
-    /// </summary>
-    static BankData()
+    public static class BankData
     {
-        var currencyISOAndCurrency = Enum.GetValues<CurrencyISO>()
-            .Zip(
-                cultureNames,
-                (currencyIso, currencyInstance) =>
-                    new KeyValuePair<CurrencyISO, Currency>(
-                        currencyIso,
-                        new Currency(
+        private static readonly string[] cultureNames = { "en-US", "be-BE", "th-TH", "sv-SE" };
+        public static readonly Dictionary<CurrencyISO, Currency> CurrencyDictionary;
+
+        private static int accountNumberPool = 111;
+
+        // TODO Should the dictionary be stored somewhere else, like BankService?
+
+        /// <summary>
+        /// Initializes the dictionary by connecting the currency type
+        /// to the currency symbol in the CultureInfo class.
+        /// </summary>
+        static BankData()
+        {
+            var currencyISOAndCurrency = Enum.GetValues<CurrencyISO>()
+                .Zip(
+                    cultureNames,
+                    (currencyIso, currencyInstance) =>
+                        new KeyValuePair<CurrencyISO, Currency>(
                             currencyIso,
-                            CultureInfo.GetCultureInfo(currencyInstance).NumberFormat.CurrencySymbol
+                            new Currency(
+                                currencyIso,
+                                CultureInfo
+                                    .GetCultureInfo(currencyInstance)
+                                    .NumberFormat.CurrencySymbol
+                            )
                         )
-                    )
-            );
+                );
 
-        CurrencyDictionary = new Dictionary<CurrencyISO, Currency>(currencyISOAndCurrency);
+            CurrencyDictionary = new Dictionary<CurrencyISO, Currency>(currencyISOAndCurrency);
 
-        // Correct to the _real_ SEK symbol:
-        CurrencyDictionary[CurrencyISO.SEK] = new Currency(CurrencyISO.SEK, "♕");
+            // Correct to the _real_ SEK symbol:
+            CurrencyDictionary[CurrencyISO.SEK] = new Currency(CurrencyISO.SEK, "♕");
+        }
+
+        public static string? GenerateAccountNumber()
+        {
+            var accountNumberString =
+                Random.Shared.Next(11111, 100000).ToString() + accountNumberPool;
+            accountNumberPool++;
+            return accountNumberString;
+        }
     }
-}
 
-public enum CurrencyISO
-{
-    USD,
-    EUR,
-    THB,
-    SEK,
+    public enum CurrencyISO
+    {
+        USD,
+        EUR,
+        THB,
+        SEK,
+    }
 }
