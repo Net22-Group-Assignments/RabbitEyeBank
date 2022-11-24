c01322027c06eb7513ea9351f28acc6aadd59a04
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-24 01:44:54 +0100

    feat: add deposit and withdraw method
    closes #38

--- RabbitEyeBank/Money/BankAccount.cs
+++ RabbitEyeBank/Money/BankAccount.cs
@@ -11,17 +11,32 @@ namespace RabbitEyeBank.Money
     /// </summary>
     public class BankAccount
     {
-        public string AccountNumber { get; init; }
+        public string? AccountNumber { get; init; }
         public string Name { get; set; }
-        public decimal AccBalance { get; set; }
+        public decimal Balance { get; set; }
         public Currency Currency { get; set; }
 
-        public BankAccount(string accountNumber, string name, decimal accBalance, Currency currency)
+        public BankAccount(string? accountNumber, string name, decimal balance, Currency currency)
         {
             AccountNumber = accountNumber;
             Name = name;
-            AccBalance = accBalance;
+            Balance = balance;
             Currency = currency;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > Balance)
+            {
+                throw new InvalidOperationException("Not enough money in the bankaccount.");
+            }
+
+            Balance -= amount;
         }
     }
 }
