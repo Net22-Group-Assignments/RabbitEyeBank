23e3686d585d6b747b72b244fd2c92e509b2ed43
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-24 02:00:10 +0100

    test: add exception test for withdraw method

--- RabbitEyeTests/BankServiceTests.cs
+++ RabbitEyeTests/BankServiceTests.cs
@@ -1,5 +1,4 @@
 using RabbitEyeBank;
-using Xunit.Sdk;
 
 namespace RabbitEyeTests
 {
---
+++ RabbitEyeTests/MoneyTransferTests.cs
@@ -0,0 +1,14 @@
+﻿using RabbitEyeBank.Money;
+
+namespace RabbitEyeTests;
+
+public class MoneyTransferTests
+{
+    private readonly BankAccount bankAccount = new("1", "A", 0m, new Currency());
+
+    [Fact]
+    public void WithdrawalWhenAmountTooLittle_ThrowsException()
+    {
+        Assert.Throws<InvalidOperationException>(() => bankAccount.Withdraw(1m));
+    }
+}
