6a45a237c60e45af9cb90ec74437e3f11f1c9957
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-26 22:05:05 +0100

    feat: add bunny to header

--- LoginDemo/UI/Windows/CustomerHeader.cs
+++ LoginDemo/UI/Windows/CustomerHeader.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitEyeBank.Services;
+using SixLabors.ImageSharp.Processing;
 using Spectre.Console;
 
 namespace LoginDemo.UI.Windows
@@ -9,8 +10,6 @@ namespace LoginDemo.UI.Windows
         protected AccountService accountService;
         protected MoneyTransferService moneyTransferService;
 
-        private readonly Grid grid;
-
         protected CustomerHeader(
             UserService userService,
             AccountService accountService,
@@ -20,33 +19,6 @@ namespace LoginDemo.UI.Windows
             this.UserService = userService;
             this.accountService = accountService;
             this.moneyTransferService = moneyTransferService;
-
-            grid = new Grid();
-            grid.AddColumns(2);
-            grid.AddRow(
-                Markup.FromInterpolated(
-                    $@"&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%/(&@@@@@@@@@@@@@@@@@@%/*%%%@@@@@@@@@@@@@@@@@@@
-&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@(###/*/#&@@@@@@@@@@((*/%%##%@@@@@@@@@@@@@@@@@@@
-&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##(//#(,,*%@@@@@@(,,*(%#####@@@@@@@@@@@@@@@@@@@
-&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%((///(/,.,*&@@&(*,*,##(###%@@@@@@@@@@@@@@@@@@@
-&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&((//((/*(%&&&&&&&&%/(#(#(#%@@@@@@@@@@@@@@@@@@@
-&&@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%(/*(%&&&&@@@@@@@@@@&&%##%@@@@@@@@@@@@@@@@@@@@
-&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%%&&&@@@@@@@@@@@@@@@@@@&&@@@@@@@@@@@@@@@@@@@@
-&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&%#&@@@@@@@@@@@@@&&&@@@@@@@@@@@@@@@@@@@@@@
-&&&@&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&%//(#&&@@@@@@@@@@&&#&@@@@@@@@@@@@@@@@@@@@@@
-&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%/*,.(%&&&&&&&&&&&&&#/(&@@@@@@@@@@@@@@@@@@@@@
-&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&%#(#*.,#%&&&&&&&&&&&#,/###&@@@@@@@@@@@@@@@@@@@
-&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&%%%&&&&&&&&&&@@@&&&&&%%&@@@@@@@@@@@@@@@@@@
-&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&%%%###%%%&&@@@@@@@@@@@@@@@@@@@@@@@@@@@
-&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@&@@&&&&&&&&&&&&%(#%(%&&&@&@@@@@@@@@@@@@@@@@@@@@@@@
-&&&&&&&@@@@@@@@@@@@@@@@@@@@@&&&@&&&&&&&&&&&&&&%##%&&&@@@@@@@@@@@@@@@@@@@@@@@@@@@
-&&&&&&&@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@@@
-@@@@@@@@@@@@@@@@@@@@@@@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&@@@@@@@@@@@@@@@@@@@@@@@
-"
-                ),
-                Markup.FromInterpolated($"{DateTime.Now}")
-            );
         }
 
         protected CustomerHeader()
@@ -59,11 +31,28 @@ namespace LoginDemo.UI.Windows
         /// <inheritdoc />
         public virtual void Show()
         {
+            //Grid grid = new Grid();
+            //grid.AddColumns(2);
             AnsiConsole.Clear();
+            int width = AnsiConsole.Profile.Width;
             showWindowStack();
             AnsiConsole.WriteLine($"Level {Level}");
+            var image = new CanvasImage(
+                $"Assets{Path.AltDirectorySeparatorChar}bunnyrabbit2xW.png"
+            );
+            image.MaxWidth(40);
+            image.PixelWidth(1);
+            image.Mutate(ctx => ctx.Contrast(2.0f));
+            AnsiConsole.Write(image);
+
+            //grid.AddRow(new FigletText("RabbitEye Bank").LeftAligned().Color(Color.Purple), image);
+            //grid.Alignment(Justify.Left);
             //AnsiConsole.Write(grid);
-            AnsiConsole.Write(new Rule());
+            AnsiConsole.Write(
+                new Rule("[bold orchid]Rabbit-Eye Bank[/]")
+                    .LeftAligned()
+                    .RuleStyle(Style.Parse("orchid"))
+            );
         }
     }
 }
