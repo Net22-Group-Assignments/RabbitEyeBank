0d331e0d1779c154845c633390764479e275e6a3
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-26 22:29:55 +0100

    feat: add overwiev table to main admin window

---
+++ LoginDemo/UI/Windows/AdminWindow.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+namespace LoginDemo.UI.Windows;
+
+public class AdminWindow : CustomerHeader
+{
+    /// <inheritdoc />
+    public override void Show()
+    {
+        base.Show();
+        var bankDataTable = new Table();
+        bankDataTable
+            .Title("Overview")
+            .AddColumns("Total Customers", "Total Accounts", "Total Transactions")
+            .AddRow(
+                Markup.FromInterpolated($"{UserService.CustomerList.Count}"),
+                Markup.FromInterpolated($"{AccountService.AccountList.Count}"),
+                Markup.FromInterpolated($"{MoneyTransferService.TransferLog}")
+            ); // TODO add total balance in bank? (bonus)
+    }
+}
