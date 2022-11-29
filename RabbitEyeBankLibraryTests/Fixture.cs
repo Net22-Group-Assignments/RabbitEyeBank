2de112612a028e9970e5d99543984203c916832b
Björn Agnemo <bjorn.agnemo@gmail.com>
2022-11-29 16:52:31 +0100

    test: add and update tests
    Tests the conversion operation.

--- RabbitEyeBankLibraryTests/Fixture.cs
+++ RabbitEyeBankLibraryTests/Fixture.cs
@@ -8,6 +8,7 @@ public class Fixture
 {
     public UserService UserService;
     public AccountService AccountService;
+    public CurrencyService CurrencyService;
     public MoneyTransferService MoneyTransferService;
 
     public Customer Customer1;
@@ -16,18 +17,27 @@ public class Fixture
     public BankAccount BankAccount2;
     public BankAccount BankAccount3;
 
+    public Currency ZorkMid;
+
     public Fixture()
     {
         AccountService = new AccountService();
         UserService = new UserService();
-        MoneyTransferService = new MoneyTransferService(UserService, AccountService);
+        CurrencyService = new CurrencyService();
+        MoneyTransferService = new MoneyTransferService(
+            UserService,
+            AccountService,
+            CurrencyService
+        );
+        ZorkMid = new Currency(CurrencyISO.SEK, "Z", 0.5m);
 
         Customer1 = new Customer("Alice", "Allison", "alice", "alice", true);
         Customer2 = new Customer("Bob", "Roberts", "bob", "bob", true);
-        BankAccount1 = new BankAccount("1234", "savings", 100m, new Currency(), Customer1);
-        BankAccount2 = new BankAccount("5678", "loan", 200m, new Currency(), Customer1);
-        BankAccount3 = new BankAccount("9012", "slush-fund", 300m, new Currency(), Customer2);
+        BankAccount1 = new BankAccount("1234", "savings", 100m, CurrencyService.Dollar, Customer1);
+        BankAccount2 = new BankAccount("5678", "loan", 200m, CurrencyService.Dollar, Customer1);
+        BankAccount3 = new BankAccount("9012", "slush-fund", 300m, ZorkMid, Customer2);
         AccountService.AddBankAccount(BankAccount1);
         AccountService.AddBankAccount(BankAccount2);
+        AccountService.AddBankAccount(BankAccount3);
     }
 }
--- RabbitEyeBankLibraryTests/MoneyTransferTests.cs
+++ RabbitEyeBankLibraryTests/MoneyTransferTests.cs
@@ -18,12 +18,27 @@ public class MoneyTransferTests : IClassFixture<Fixture>
             fixture.BankAccount1,
             fixture.BankAccount2,
             100m,
-            new Currency(),
-            new Currency()
+            fixture.CurrencyService.Dollar,
+            fixture.CurrencyService.Dollar
         );
 
         fixture.MoneyTransferService.TransferMoney(transfer);
         Assert.Equal(0m, fixture.BankAccount1.Balance);
         Assert.Equal(300m, fixture.BankAccount2.Balance);
+    }
+
+    [Fact]
+    public void TransferBetweenTwoDifferentCurrencies()
+    {
+        decimal oldBalance = fixture.BankAccount3.Balance;
+        var transfer = fixture.MoneyTransferService.CreateTransfer(
+            fixture.BankAccount1,
+            fixture.BankAccount3,
+            100m,
+            fixture.CurrencyService.Dollar,
+            fixture.ZorkMid
+        );
+        fixture.MoneyTransferService.TransferMoney(transfer);
+        Assert.Equal(oldBalance + 100 / fixture.ZorkMid.DollarValue, fixture.BankAccount3.Balance);
     }
 }
